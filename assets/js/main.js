1a900c1f5cc8efac04adfbccc7d3837452723061
Giovanni <giov@nnisouza.com>
2015-09-22 18:38:28 -0300

    header

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,32 @@ function actions() {
     cheet('s h a k e s p e a r e', function () {
       alert('Doubt thou the stars are fire; \nDoubt that the sun doth move; \nDoubt truth to be a liar; \nBut never doubt I love. \n                                                        - William Shakespeare');
     });
+    
+    $('#fakeButton').click(function(){
+        $('.yellowTape').animate({
+            width: '100%'
+        }, 500);
+        $('#citiesMonument').animate({
+            opacity: 0
+        }, 400);
+
+        setTimeout(function() {
+            $('.yellowTape').animate({
+                height: '129px',
+                'border-color': '#b59017'
+            }, 500);
+
+            $('.head').addClass('fit');
+
+            setTimeout(function() {
+                $('.yellowTape').css({
+                    'border-top-color': '#b59017'
+                });
+            }, 510);
+
+        }, 550);
+    });
+    
 }
 function afterLoaded() {
     smoothScroll();
